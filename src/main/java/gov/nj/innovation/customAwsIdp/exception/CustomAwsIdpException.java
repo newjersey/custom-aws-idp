1e135166ac126a1383246ee56d67bcf4c11b26ad
Case Walker <case.b.walker@gmail.com>
2023-10-03 13:06:45 -0400

    Start fresh on Main

--- src/main/java/gov/nj/innovation/customAwsIdp/exception/CustomAwsIdpException.java
+++
@@ -1,7 +0,0 @@
-package gov.nj.innovation.customAwsIdp.exception;
-
-public class CustomAwsIdpException extends RuntimeException {
-    public CustomAwsIdpException(String errorMessage, Throwable err) {
-        super(errorMessage, err);
-    }
-}
--- src/main/java/gov/nj/innovation/customAwsIdp/exception/KeyGenerationException.java
+++
@@ -1,7 +0,0 @@
-package gov.nj.innovation.customAwsIdp.exception;
-
-public class KeyGenerationException extends CustomAwsIdpException {
-    public KeyGenerationException(String errorMessage, Throwable err) {
-        super(errorMessage, err);
-    }
-}
