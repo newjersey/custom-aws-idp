a5f801da99d10f0ab694eac7bf7254db8d25c121
Case Walker <case.b.walker@gmail.com>
2024-12-08 20:35:31 -0500

    Rearrange dependencies, remove extra logs

--- src/main/java/gov/nj/innovation/customAwsIdp/util/SsmClientWrapper.java
+++ src/main/java/gov/nj/innovation/customAwsIdp/util/SsmClientWrapper.java
@@ -1,7 +1,5 @@
 package gov.nj.innovation.customAwsIdp.util;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import software.amazon.awssdk.services.ssm.SsmClient;
 import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
 import software.amazon.awssdk.services.ssm.model.GetParameterResponse;
@@ -13,8 +11,6 @@ import software.amazon.awssdk.services.ssm.model.GetParameterResponse;
  */
 public class SsmClientWrapper {
 
-    private static final Logger logger = LogManager.getLogger(SsmClientWrapper.class);
-
     /**
      * Encapsulate the Amazon types; consume a String and output its SSM value.
      *
@@ -22,14 +18,12 @@ public class SsmClientWrapper {
      * @return The SSM parameter value.
      */
     public static String getParameterByName(final SsmClient ssmClient, final String parameterName) {
-        logger.info("cw - ssmClientWrapper getting parameter {}", parameterName);
         final GetParameterRequest request = GetParameterRequest.builder()
                 .name(parameterName)
                 .withDecryption(true)
                 .build();
-        logger.info("cw - ssmClientWrapper running api");
         final GetParameterResponse response = ssmClient.getParameter(request);
-        logger.info("cw - ssmClientWrapper got response {}", response);
+
         return response.parameter().value();
     }
 }
